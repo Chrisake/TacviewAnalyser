eeee63424a0c971a4f0e216c465d74aeb469c430
Christoforos Sakellaris <chrissakellaris7@gmail.com>
2025-04-09 23:16:09 +0200

    TA-8: Load File with chunks refactor
    Remove Progress indicator from multithreaded load

--- TA_Lib/src/tac_file.cpp
+++ TA_Lib/src/tac_file.cpp
@@ -2,7 +2,6 @@
 
 #include <cctype>
 #include <fstream>
-#include <indicators/progress_bar.hpp>
 #include <iostream>
 #include <memory>
 #include <regex>
@@ -24,7 +23,7 @@ std::filesystem::path *TacFile::PrepareACMIFile(const std::filesystem::path &fil
   if (std::regex_search(filepath_str, match, double_extension)) {
     if (match[0].str() == ".zip.acmi") {
       // Need to unzip it
-      std::filesystem::path base_path = std::filesystem::temp_directory_path() / "TacView_Analyser";
+      std::filesystem::path base_path = std::filesystem::temp_directory_path() / "TacView_Analyser" / getRandomGUID();
       if (unzip_file(filepath, base_path) != 0) {
         std::cerr << "Failed to unzip file\n";
         return nullptr;
@@ -110,56 +109,22 @@ bool TacFile::LoadFile(const std::filesystem::path &filepath) { return LoadFileW
 
 #define AVG_CHARACTERS_PER_LINE 50
 
-std::regex pattern = std::regex(
-    R"((?:^|\n)(#?)(-?[0-9a-f.]+)(?:,T=([-\d.|]*)(?:$|,Type=((?:Weapon\+Missile|Air\+FixedWing)[^,]*),Name=([^,]*)(?:,Pilot=([^,]*))?(?:,Group=([^,]*))?,Color=(Blue|Red)[^\n]*)?)?(?=$|\n))",
-    std::regex_constants::optimize);
-
-static void progressThread(const std::vector<size_t> &prog,
-                           const size_t               total_size,
-                           size_t                    &lines_processed,
-                           size_t                    &total_lines,
-                           bool                      &done) {
-  using namespace indicators;
-  ProgressBar progress{
-      option::BarWidth{80},
-      option::Start{"["},
-      option::End{"]"},
-      option::Fill{"="},
-      option::Lead{">"},
-      option::ForegroundColor{Color::white},
-      option::ShowElapsedTime{true},
-      option::ShowRemainingTime{true},
-      option::ShowPercentage{true},
-      option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
-      option::MaxProgress(total_size + total_lines),
-  };
-
-  bool file_parsed = false;
-
+static void progressThread(const std::vector<Thread_Payload> &thread_payloads,
+                           const size_t                      &lines_processed,
+                           size_t                            &current_progress,
+                           size_t                            &total_progress) {
   while (true) {
-    size_t current_prog = 0;
-    size_t total_prog = total_size + total_lines;
-    if (done) {
-      current_prog = total_prog;
-    } else {
-      if (file_parsed) {
-        current_prog += total_size;
-      } else {
-        for (const auto &p : prog) {
-          current_prog += p;
-        }
-        if (current_prog >= total_size) {
-          file_parsed = true;
-        }
-      }
-      current_prog += lines_processed;
+    size_t total_prog = 0;
+    size_t cur_prog = lines_processed;
+    for (const auto &payload : thread_payloads) {
+      size_t est_remaining = (payload.endIdx - payload.startIdx - payload.progress) / AVG_CHARACTERS_PER_LINE;
+      cur_prog += payload.lines.size();
+      total_prog += 2 * (payload.lines.size() + est_remaining);  // 2 for each line (one parsing and one processing)
     }
-    progress.set_option(option::MaxProgress(total_prog));
-    progress.set_progress(static_cast<size_t>(current_prog));
-    if (progress.is_completed() || done) {
-      break;
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    current_progress = cur_prog;
+    total_progress = total_prog;
+    if (current_progress >= total_progress) break;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 }
 
@@ -186,16 +151,15 @@ bool TacFile::LoadFileWithChunks(const std::filesystem::path &filepath) {
       buffer.clear(); buffer << file.rdbuf(); fileContents = buffer.str();)
 
   std::cout << "File read time: " << file_read_time / 1000000.0f << "s\n";
-  const auto               processor_count = std::thread::hardware_concurrency();
-  const auto               chunk_size = fileContents.size() / processor_count;
-  std::vector<std::thread> threads;
+  const auto                  processor_count = std::thread::hardware_concurrency();
+  const auto                  chunk_size = fileContents.size() / processor_count;
+  std::vector<std::thread>    threads;
+  std::vector<Thread_Payload> payloads;
+  size_t                      prevEnd = 0;
   threads.reserve(processor_count);
-  std::vector<std::pair<size_t, size_t>>              chunks;
-  std::vector<size_t>                                 progress(processor_count, 0);
-  std::vector<std::vector<std::unique_ptr<FileLine>>> file_lines;
-  chunks.reserve(processor_count);
-  file_lines.reserve(processor_count);
-  size_t prevEnd = 0;
+  payloads.reserve(processor_count);
+
+  // Skip the first two lines
   for (uint8_t i = 0; i < 2; i++) {
     prevEnd = fileContents.find('\n', prevEnd);
   }
@@ -206,52 +170,52 @@ bool TacFile::LoadFileWithChunks(const std::filesystem::path &filepath) {
       nextEnd++;
     }
     nextEnd++;
-    chunks.emplace_back(prevEnd, nextEnd);
-    file_lines.emplace_back();
-    file_lines.back().reserve(chunk_size / AVG_CHARACTERS_PER_LINE);
+
+    Thread_Payload payload = {
+        .lines = std::vector<std::unique_ptr<FileLine>>(),
+        .startIdx = prevEnd,
+        .endIdx = nextEnd,
+        .progress = 0,
+    };
+    payload.lines.reserve(chunk_size / AVG_CHARACTERS_PER_LINE);
+    payloads.emplace_back(std::move(payload));
     prevEnd = nextEnd;
   }
 
-  size_t total_size = fileContents.size() - chunks[0].first;
   size_t lines_processed = 0;
-  size_t total_lines = total_size / AVG_CHARACTERS_PER_LINE;
-  size_t real_lines = 0;
-  bool   finished = false;
+  size_t current_progress = 0;
+  size_t total_progress = 0;
 
-  std::thread progress_thread(progressThread, std::cref(progress), total_size, std::ref(lines_processed),
-                              std::ref(total_lines), std::ref(finished));
+  std::thread progress_thread(progressThread, std::cref(payloads), std::cref(lines_processed),
+                              std::ref(current_progress), std::ref(total_progress));
 
   for (size_t i = 0; i < processor_count; i++) {
-    threads.emplace_back(TacFile::AnalyseChunk, std::cref(fileContents), std::ref(file_lines[i]), chunks[i].first,
-                         chunks[i].second, std::ref(progress[i]));
+    threads.emplace_back(TacFile::AnalyseChunk, std::cref(fileContents), std::ref(payloads[i]));
   }
 
   std::chrono::milliseconds cur_time;
 
   for (size_t i = 0; i < processor_count; i++) {
     threads[i].join();
-    real_lines += file_lines[i].size();
-    total_lines = real_lines + (processor_count - 1 - i) * (total_size / (processor_count * AVG_CHARACTERS_PER_LINE));
-    for (const auto &line : file_lines[i]) {
+    for (const auto &line : payloads[i].lines) {
       line->ProcessLine(active_objects, tracked_objects, pilot_runs, cur_time);
     }
-    lines_processed += file_lines[i].size();
+    lines_processed += payloads[i].lines.size();
   }
 
-  finished = true;
   progress_thread.join();
   return true;
 }
 
-void TacFile::AnalyseChunk(const std::string                      &fileContents,
-                           std::vector<std::unique_ptr<FileLine>> &lines,
-                           size_t                                  startIdx,
-                           size_t                                  endIdx,
-                           size_t                                 &progress) {
-  std::sregex_iterator it(fileContents.begin() + startIdx, fileContents.begin() + endIdx, pattern);
+std::regex pattern = std::regex(
+    R"((?:^|\n)(#?)(-?[0-9a-f.]+)(?:,T=([-\d.|]*)(?:$|,Type=((?:Weapon\+Missile|Air\+FixedWing)[^,]*),Name=([^,]*)(?:,Pilot=([^,]*))?(?:,Group=([^,]*))?,Color=(Blue|Red)[^\n]*)?)?(?=$|\n))",
+    std::regex_constants::optimize);
+
+void TacFile::AnalyseChunk(const std::string &fileContents, Thread_Payload &payload) {
+  std::sregex_iterator it(fileContents.begin() + payload.startIdx, fileContents.begin() + payload.endIdx, pattern);
   std::sregex_iterator end;
   while (it != end) {
-    progress = it->position() + it->length();
+    payload.progress = it->position() + it->length() - payload.startIdx;
     // Group 1: # Symbol (timestamp)
     // Group 2: ID / Time(seconds)
     // Group 3: Location String
@@ -263,21 +227,22 @@ void TacFile::AnalyseChunk(const std::string                      &fileContents,
     std::smatch match = *it;
     if (match[1].length() > 0) {
       // Its a timestamp
-      lines.emplace_back(new TimestampLine(match[2].str()));
+      payload.lines.emplace_back(new TimestampLine(match[2].str()));
     } else {
       int32_t id = std::stoi(match[2].str(), nullptr, 16);
       if (id < 0) {
         // Its a despawn
-        lines.emplace_back(new DespawnLine(-id));
+        payload.lines.emplace_back(new DespawnLine(-id));
       } else if (match[4].length() > 0) {
         // Its a spawn
-        lines.emplace_back(new SpawnLine(id, match[3].str(), match[5].str(), match[4].str(), match[6].str(),
-                                         match[7].str(), match[8].str()));
+        payload.lines.emplace_back(new SpawnLine(id, match[3].str(), match[5].str(), match[4].str(), match[6].str(),
+                                                 match[7].str(), match[8].str()));
       } else {
         // Its an update
-        lines.emplace_back(new UpdateLine(id, match[3].str()));
+        payload.lines.emplace_back(new UpdateLine(id, match[3].str()));
       }
     }
     ++it;
   }
+  payload.progress = payload.endIdx - payload.startIdx;
 }
